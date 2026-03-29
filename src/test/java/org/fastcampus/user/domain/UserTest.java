be68c839f1f1de388c82a05a54827f50199b9736
jy-hwang <stratojy@gmail.com>
2026-03-29 18:16:43 +0900

    feat. UserTest 만들기 2
    1. follow & unfollow 테스트
    2. BeforeEach : 테스트 메서드 실행 이전에 수행됨.

--- src/test/java/org/fastcampus/user/domain/UserTest.java
+++ src/test/java/org/fastcampus/user/domain/UserTest.java
@@ -1,13 +1,20 @@
 package org.fastcampus.user.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
   private final UserInfo userInfo = new UserInfo("test", "");
-  private final User user1 = new User(1L, userInfo);
-  private final User user2 = new User(2L, userInfo);
+  private User user1;
+  private User user2;
+
+  @BeforeEach
+  void init() {
+    user1 = new User(1L, userInfo);
+    user2 = new User(2L, userInfo);
+  }
 
   @Test
   void givenTwoUser_whenEquals_thenReturnFalse() {
@@ -51,5 +58,44 @@ class UserTest {
 
     // then
     assertEquals(hashCode1, sameUserHashCode);
+  }
+
+  @Test
+  void givenTwoUser_whenUser1FollowUser2_thenIncreaseUserCount() {
+    // when
+    user1.follow(user2);
+
+    // then
+    assertEquals(1, user1.followingCount());
+    assertEquals(0, user1.followerCount());
+    assertEquals(0, user2.followingCount());
+    assertEquals(1, user2.followerCount());
+  }
+
+  @Test
+  void givenTwoUserAndUser1FollowUser2_whenUnfollow_thenDecreaseUserCount() {
+    // given
+    user1.follow(user2);
+
+    // when
+    user1.unfollow(user2);
+
+    // then
+    assertEquals(0, user1.followingCount());
+    assertEquals(0, user1.followerCount());
+    assertEquals(0, user2.followingCount());
+    assertEquals(0, user2.followerCount());
+  }
+
+  @Test
+  void givenTwoUser_whenUnfollow_thenDecreaseUserCount() {
+    // when
+    user1.unfollow(user2);
+
+    // then
+    assertEquals(0, user1.followingCount());
+    assertEquals(0, user1.followerCount());
+    assertEquals(0, user2.followingCount());
+    assertEquals(0, user2.followerCount());
   }
 }
