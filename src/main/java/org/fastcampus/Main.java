82e3e1e2345af4e80c7866cefac4112c00a5c3d8
jy-hwang <stratojy@gmail.com>
2026-04-22 19:01:11 +0900

    chore. formatting 변경

--- src/main/java/org/fastcampus/Main.java
+++ src/main/java/org/fastcampus/Main.java
@@ -2,6 +2,6 @@ package org.fastcampus;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.printf("Hello and welcome!");
+    System.out.print("Hello and welcome!");
   }
 }
