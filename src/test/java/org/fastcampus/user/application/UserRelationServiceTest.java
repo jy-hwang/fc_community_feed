3be6277e07e2bc3d228298ecb9ed662065a1509a
jy-hwang <stratojy@gmail.com>
2026-04-24 10:13:57 +0900

    test. Follow 기능 테스트

--- src/test/java/org/fastcampus/user/application/UserRelationServiceTest.java
+++ src/test/java/org/fastcampus/user/application/UserRelationServiceTest.java
@@ -1,9 +1,17 @@
 package org.fastcampus.user.application;
 
+import org.fastcampus.user.application.dto.CreateUserRequestDto;
+import org.fastcampus.user.application.dto.FollowUserRequestDto;
 import org.fastcampus.user.application.interfaces.UserRelationRepository;
 import org.fastcampus.user.application.interfaces.UserRepository;
+import org.fastcampus.user.domain.User;
 import org.fastcampus.user.repository.FakeUserRelationRepository;
 import org.fastcampus.user.repository.FakeUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserRelationServiceTest {
   private final UserRepository userRepository = new FakeUserRepository();
@@ -11,4 +19,47 @@ public class UserRelationServiceTest {
   private final UserRelationRepository userRelationRepository = new FakeUserRelationRepository();
   private final UserRelationService userRelationService = new UserRelationService(userService, userRelationRepository);
 
+  private User user1;
+  private User user2;
+
+  private FollowUserRequestDto requestDto;
+
+  @BeforeEach
+  void init() {
+    CreateUserRequestDto dto = new CreateUserRequestDto("test", "");
+    this.user1 = userService.createUser(dto);
+    this.user2 = userService.createUser(dto);
+
+    this.requestDto = new FollowUserRequestDto(user1.getId(), user2.getId());
+  }
+
+  @Test
+  void givenCreateTwoUser_whenFollow_thenUserFollowSaved() {
+    // given
+
+    // when
+    userRelationService.follow(requestDto);
+
+    // then
+    assertEquals(1, user1.followingCount());
+    assertEquals(1, user2.followerCount());
+  }
+
+  @Test
+  void givenCreateTwoUserFollowed_whenFollow_thenUserThrowError() {
+    // given
+    userRelationService.follow(requestDto);
+
+    // when & then
+    assertThrows(IllegalArgumentException.class, () -> userRelationService.follow(requestDto));
+  }
+
+  @Test
+  void givenCreateOneUser_whenFollowSelf_thenUserThrowError() {
+    // given
+    FollowUserRequestDto sameUser = new FollowUserRequestDto(user1.getId(), user1.getId());
+
+    // when & then
+    assertThrows(IllegalArgumentException.class, () -> userRelationService.follow(sameUser));
+  }
 }
