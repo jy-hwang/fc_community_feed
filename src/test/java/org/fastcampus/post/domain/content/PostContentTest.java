53a9ab84594e2be0516f624c9d16b392cf9c3092
jy-hwang <stratojy@gmail.com>
2026-04-25 15:25:47 +0900

    test. PostContentTest - Updated

--- src/test/java/org/fastcampus/post/domain/content/PostContentTest.java
+++ src/test/java/org/fastcampus/post/domain/content/PostContentTest.java
@@ -55,4 +55,63 @@ class PostContentTest {
     // when & then
     assertThrows(IllegalArgumentException.class, () -> new PostContent(value));
   }
+
+  @Test
+  void givenContentLengthIsOk_whenUpdated_thenNotThrowError() {
+    // given
+    String text = "this is a test content";
+    PostContent postContent = new PostContent(text);
+
+    // when & then
+    postContent.updateContent("this is a updated content");
+  }
+
+  @Test
+  void givenContentLengthIsOk_whenUpdated_thenReturnUpdatedContent() {
+    // given
+    String text = "this is a test content";
+    PostContent postContent = new PostContent(text);
+
+    // when
+    String updatedContent = "this is a updated content";
+    postContent.updateContent(updatedContent);
+
+    // then
+    assertEquals(updatedContent, postContent.contentText);
+  }
+
+
+  @Test
+  void givenContentLengthIsOver_whenUpdated_thenThrowsError() {
+    // given
+    String text = "this is a test content";
+    PostContent postContent = new PostContent(text);
+
+    // when & then
+    String value = "a".repeat(501);
+    assertThrows(IllegalArgumentException.class, () -> postContent.updateContent(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"뷁, 닭, 긁, 삵, 슳"})
+  void givenContentLengthIsOverAndKorean_whenUpdated_thenThrowsError(String koreanWord) {
+    // given
+    String text = "this is a test content";
+    PostContent postContent = new PostContent(text);
+
+    // when & then
+    String value = koreanWord.repeat(501);
+    assertThrows(IllegalArgumentException.class, () -> postContent.updateContent(value));
+  }
+
+  @Test
+  void givenContentLengthIsUnder_whenUpdated_thenThrowsError() {
+    // given
+    String text = "this is a test content";
+    PostContent postContent = new PostContent(text);
+
+    // when & then
+    String content = "a".repeat(4);
+    assertThrows(IllegalArgumentException.class, () -> new PostContent(content));
+  }
 }
