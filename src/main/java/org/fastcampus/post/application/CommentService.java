e5c7e3326751bff48f89777be5cd19ab4a5a1690
jyhwang <stratojy@gmail.com>
2026-05-26 23:40:51 +0900

    feat. 댓글 생성 기능 구현
    1. 생성자에 주입할 객체 선언
    2. RequestDto 생성
    3. 댓글 생성에 사용할 '정적 팩토리 메서드' 만들기
    4. 댓글 생성 기능 구현

--- src/main/java/org/fastcampus/post/application/CommentService.java
+++ src/main/java/org/fastcampus/post/application/CommentService.java
@@ -1,17 +1,34 @@
 package org.fastcampus.post.application;
 
+import org.fastcampus.post.application.dto.CreateCommentRequestDto;
+import org.fastcampus.post.application.dto.UpdateCommentRequestDto;
 import org.fastcampus.post.application.interfaces.CommentRepository;
+import org.fastcampus.post.domain.Post;
 import org.fastcampus.post.domain.comment.Comment;
+import org.fastcampus.user.application.UserService;
+import org.fastcampus.user.domain.User;
 
 public class CommentService {
   private final CommentRepository commentRepository;
+  private final UserService userService;
+  private final PostService postService;
 
-  public CommentService(CommentRepository commentRepository) {
+  public CommentService(CommentRepository commentRepository, UserService userService, PostService postService) {
     this.commentRepository = commentRepository;
+    this.userService = userService;
+    this.postService = postService;
   }
 
   public Comment getComment(Long commentId) {
     return commentRepository.findById(commentId).orElseThrow(IllegalAccessError::new);
+  }
+
+  public Comment createComment(CreateCommentRequestDto dto) {
+    Post post = postService.getPost(dto.postId());
+    User user = userService.getUser(dto.userId());
+
+    Comment comment = Comment.createComment(post, user, dto.content());
+    return commentRepository.save(comment);
   }
 }
 
--- src/main/java/org/fastcampus/post/application/interfaces/CommentRepository.java
+++ src/main/java/org/fastcampus/post/application/interfaces/CommentRepository.java
@@ -5,5 +5,7 @@ import org.fastcampus.post.domain.comment.Comment;
 import java.util.Optional;
 
 public interface CommentRepository {
+  Comment save(Comment comment);
+
   Optional<Comment> findById(Long commentId);
 }
