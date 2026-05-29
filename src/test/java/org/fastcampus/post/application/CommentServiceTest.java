263fd171384681375265af75f6144eb823fe6ef8
jyhwang <stratojy@gmail.com>
2026-05-29 23:13:37 +0900

    feat. 댓글 수정 테스트

--- src/test/java/org/fastcampus/post/application/CommentServiceTest.java
+++ src/test/java/org/fastcampus/post/application/CommentServiceTest.java
@@ -1,5 +1,6 @@
 package org.fastcampus.post.application;
 
+import org.fastcampus.post.application.dto.UpdateCommentRequestDto;
 import org.fastcampus.post.domain.comment.Comment;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,20 @@ public class CommentServiceTest extends PostApplicationTestTemplate {
     // then
     String content = comment.getContent();
     assertEquals(commentContentText, content);
+  }
+
+  @Test
+  void givenCreateComment_whenUpdateCommment_thenReturnUpdatedComment(){
+    // given
+    Comment comment = commentService.createComment(commentRequestDto);
+
+    // when
+    UpdateCommentRequestDto updateCommentRequestDto = new UpdateCommentRequestDto(comment.getId(), user.getId(), "updated comment");
+    Comment updatedComment = commentService.updateComment(updateCommentRequestDto);
+
+    // then
+    assertEquals(comment.getId(), updatedComment.getId());
+    assertEquals(comment.getAuthor(), updatedComment.getAuthor());
+    assertEquals(comment.getContent(), updatedComment.getContent());
   }
 }
