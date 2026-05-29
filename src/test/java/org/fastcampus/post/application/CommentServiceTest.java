33eb0cdba21a8ed5c9c1553e5650742ef85adef1
jyhwang <stratojy@gmail.com>
2026-05-29 23:21:32 +0900

    feat. 댓글 like / unlike 테스트

--- src/test/java/org/fastcampus/post/application/CommentServiceTest.java
+++ src/test/java/org/fastcampus/post/application/CommentServiceTest.java
@@ -1,5 +1,6 @@
 package org.fastcampus.post.application;
 
+import org.fastcampus.post.application.dto.LikeRequestDto;
 import org.fastcampus.post.application.dto.UpdateCommentRequestDto;
 import org.fastcampus.post.domain.comment.Comment;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ public class CommentServiceTest extends PostApplicationTestTemplate {
   }
 
   @Test
-  void givenCreateComment_whenUpdateCommment_thenReturnUpdatedComment(){
+  void givenCreateComment_whenUpdateCommment_thenReturnUpdatedComment() {
     // given
     Comment comment = commentService.createComment(commentRequestDto);
 
@@ -31,5 +32,32 @@ public class CommentServiceTest extends PostApplicationTestTemplate {
     assertEquals(comment.getId(), updatedComment.getId());
     assertEquals(comment.getAuthor(), updatedComment.getAuthor());
     assertEquals(comment.getContent(), updatedComment.getContent());
+  }
+
+  @Test
+  void givenComment_whenLikeComment_thenReturnCommentWithLike() {
+    // given
+    Comment comment = commentService.createComment(commentRequestDto);
+
+    // when
+    LikeRequestDto likeRequestDto = new LikeRequestDto(comment.getId(), otherUser.getId());
+    commentService.likeComment(likeRequestDto);
+
+    // then
+    assertEquals(1, comment.getLikeCounter());
+  }
+
+  @Test
+  void givenCommentLiked_whenUnlikeComment_thenReturnCommentWithoutLike() {
+    // given
+    Comment comment = commentService.createComment(commentRequestDto);
+    LikeRequestDto likeRequestDto = new LikeRequestDto(comment.getId(), otherUser.getId());
+    commentService.likeComment(likeRequestDto);
+
+    // when
+    commentService.unlikeComment(likeRequestDto);
+
+    // then
+    assertEquals(0, comment.getLikeCounter());
   }
 }
