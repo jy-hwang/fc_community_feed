59a56afa00848ab163e23edfaea1a08a0da3a3a1
jyhwang <stratojy@gmail.com>
2026-05-29 22:08:10 +0900

    feat. 속성 공통화
    1. 접근 제어자를 변경하여, 속성을 공통화하였음
    2. 그리고 해당 객체를 상속받아서 테스트 진행

--- src/test/java/org/fastcampus/post/application/PostServiceTest.java
+++ src/test/java/org/fastcampus/post/application/PostServiceTest.java
@@ -1,26 +1,14 @@
 package org.fastcampus.post.application;
 
-import org.fastcampus.fake.FakeObjectFactory;
-import org.fastcampus.post.application.dto.CreatePostRequestDto;
 import org.fastcampus.post.application.dto.LikeRequestDto;
 import org.fastcampus.post.application.dto.UpdatePostRequestDto;
 import org.fastcampus.post.domain.Post;
 import org.fastcampus.post.domain.content.PostPublicationState;
-import org.fastcampus.user.application.UserService;
-import org.fastcampus.user.application.dto.CreateUserRequestDto;
-import org.fastcampus.user.domain.User;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PostServiceTest {
-  private final UserService userService = FakeObjectFactory.getUserService();
-  private final PostService postService = FakeObjectFactory.getPostService();
-
-  private final User user = userService.createUser(new CreateUserRequestDto("user1", null));
-  private final User otherUser = userService.createUser(new CreateUserRequestDto("user1", null));
-
-  private final CreatePostRequestDto dto = new CreatePostRequestDto(user.getId(), "this is test Content", PostPublicationState.PUBLIC);
+public class PostServiceTest extends PostApplicationTestTemplate {
 
   @Test
   void givenPostRequestDto_whenCreate_thenReturnPost() {
