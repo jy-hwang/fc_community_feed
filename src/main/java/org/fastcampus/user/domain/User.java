43ebd0301a6c310fca562f4b533031e881377a2c
jy-hwang <stratojy@gmail.com>
2026-06-26 15:53:05 +0900

    feat. User 와 UserEntity 를 분리
    1. 관리해야할 객체가 늘어나는 것이 단점
    2. DB와 비즈니스 로직의 완전한 분리
    3. 유연함
    4. Lombok 어노테이션을 활용(Builder 등)

--- src/main/java/org/fastcampus/user/domain/User.java
+++ src/main/java/org/fastcampus/user/domain/User.java
@@ -1,14 +1,20 @@
 package org.fastcampus.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.fastcampus.common.domain.PositiveIntegerCounter;
 
 import java.util.Objects;
 
+@Getter
+@AllArgsConstructor
+@Builder
 public class User {
-  private final Long id;
-  private final UserInfo info;
-  private final PositiveIntegerCounter followingCounter;
-  private final PositiveIntegerCounter followerCounter;
+  private Long id;
+  private UserInfo info;
+  private PositiveIntegerCounter followingCounter;
+  private PositiveIntegerCounter followerCounter;
 
   public User(Long id, UserInfo userInfo) {
     if (userInfo == null) {
@@ -63,10 +69,6 @@ public class User {
     return Objects.hashCode(id);
   }
 
-  public Long getId() {
-    return id;
-  }
-
   public int followerCount() {
     return followerCounter.getCount();
   }
@@ -75,7 +77,11 @@ public class User {
     return followingCounter.getCount();
   }
 
-  public UserInfo getInfo() {
-    return info;
+  public String getProfileImage() {
+    return info.getProfileImageUrl();
+  }
+
+  public String getName() {
+    return info.getName();
   }
 }
