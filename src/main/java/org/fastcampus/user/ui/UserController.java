1f0376289f9b71d93ce2515064ae53c2f418b6a9
jy-hwang <stratojy@gmail.com>
2026-06-27 19:06:16 +0900

    feat. 서비스 API 만들기 - 리스트 following / follower

--- src/main/java/org/fastcampus/user/ui/UserController.java
+++ src/main/java/org/fastcampus/user/ui/UserController.java
@@ -4,21 +4,35 @@ import lombok.RequiredArgsConstructor;
 import org.fastcampus.common.ui.Response;
 import org.fastcampus.user.application.UserService;
 import org.fastcampus.user.application.dto.CreateUserRequestDto;
+import org.fastcampus.user.application.dto.GetUserListResponseDto;
 import org.fastcampus.user.domain.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.fastcampus.user.repository.jpa.JpaUserListQueryRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
   private final UserService userService;
+  private final JpaUserListQueryRepository userListQueryRepository;
 
   @PostMapping
   public Response<Long> createUser(@RequestBody CreateUserRequestDto dto) {
     User user = userService.createUser(dto);
     return Response.ok(user.getId());
+  }
+
+  @GetMapping("{userId}/following")
+  public Response<List<GetUserListResponseDto>> getFollowingList(@PathVariable(name = "userId") Long userId) {
+    List<GetUserListResponseDto> result = userListQueryRepository.getFollowingUserList(userId);
+    return Response.ok(result);
+  }
+
+  @GetMapping("{userId}/follower")
+  public Response<List<GetUserListResponseDto>> getFollowerList(@PathVariable(name = "userId") Long userId) {
+    List<GetUserListResponseDto> result = userListQueryRepository.getFollowerUserList(userId);
+    return Response.ok(result);
   }
 }
