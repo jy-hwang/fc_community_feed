166cabf5f323c716fb8ac59c15e78239fa9a8808
jy-hwang <stratojy@gmail.com>
2026-06-27 20:18:20 +0900

    feat. 서비스 API 만들기 - 게시글
    1. 게시글 작성 / 수정
    2. jpa의 save() 가 의도치 않은 DB transaction 을 추가로 수행해서 update 쿼리를 작성해서 실행하는 것으로 수행.

---
+++ src/main/java/org/fastcampus/post/repository/jpa/JpaPostRepository.java
@@ -0,0 +1,17 @@
+package org.fastcampus.post.repository.jpa;
+
+
+import org.fastcampus.post.repository.entity.post.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface JpaPostRepository extends JpaRepository<PostEntity, Long> {
+  @Modifying
+  @Query(value = " UPDATE PostEntity pe" +
+      "  SET pe.content = :#{#postEntity.getContent()}" +
+      ", pe.state = :#{#postEntity.getState()}" +
+      ", pe.uptDt = now()" +
+      " WHERE pe.id = :#{#postEntity.getId()} ")
+  void updatePostEntity(PostEntity postEntity);
+}
---
+++ src/main/java/org/fastcampus/post/ui/PostController.java
@@ -0,0 +1,29 @@
+package org.fastcampus.post.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.fastcampus.common.ui.Response;
+import org.fastcampus.post.application.PostService;
+import org.fastcampus.post.application.dto.CreatePostRequestDto;
+import org.fastcampus.post.application.dto.LikeRequestDto;
+import org.fastcampus.post.application.dto.UpdatePostRequestDto;
+import org.fastcampus.post.domain.Post;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+  private final PostService postService;
+
+  @PostMapping
+  public Response<Long> createPost(@RequestBody CreatePostRequestDto dto) {
+    Post post = postService.createPost(dto);
+    return Response.ok(post.getId());
+  }
+
+  @PatchMapping("/{postId}")
+  public Response<Long> updatePost(@PathVariable(name = "postId") Long postId, @RequestBody UpdatePostRequestDto dto) {
+    Post post = postService.updatePost(postId, dto);
+    return Response.ok(post.getId());
+  }
+}
