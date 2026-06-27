f65de1db1debd4b5e1732e67e7b7a00956589280
jy-hwang <stratojy@gmail.com>
2026-06-27 22:06:44 +0900

    feat. 서비스 API 만들기 - 댓글
    1. 댓글 작성 / 수정

---
+++ src/main/java/org/fastcampus/post/repository/CommentRepositoryImpl.java
@@ -0,0 +1,34 @@
+package org.fastcampus.post.repository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.fastcampus.post.application.interfaces.CommentRepository;
+import org.fastcampus.post.domain.comment.Comment;
+import org.fastcampus.post.repository.entity.comment.CommentEntity;
+import org.fastcampus.post.repository.jpa.JpaCommentRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryImpl implements CommentRepository {
+  private final JpaCommentRepository jpaCommentRepository;
+
+  @Override
+  @Transactional
+  public Comment save(Comment comment) {
+    CommentEntity commentEntity = new CommentEntity(comment);
+    if (comment.getId() != null) {
+      jpaCommentRepository.updateCommentEntity(commentEntity);
+      return comment;
+    }
+    commentEntity = jpaCommentRepository.save(commentEntity);
+    return commentEntity.toComment();
+  }
+
+  @Override
+  public Comment findById(Long commentId) {
+    CommentEntity commentEntity = jpaCommentRepository.findById(commentId).orElse(null);
+
+    return commentEntity.toComment();
+  }
+}
---
+++ src/main/java/org/fastcampus/post/ui/CommentController.java
@@ -0,0 +1,29 @@
+package org.fastcampus.post.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.fastcampus.common.ui.Response;
+import org.fastcampus.post.application.CommentService;
+import org.fastcampus.post.application.dto.CreateCommentRequestDto;
+import org.fastcampus.post.application.dto.LikeRequestDto;
+import org.fastcampus.post.application.dto.UpdateCommentRequestDto;
+import org.fastcampus.post.domain.comment.Comment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+public class CommentController {
+  private final CommentService commentService;
+
+  @PostMapping
+  public Response<Long> createComment(@RequestBody CreateCommentRequestDto dto) {
+    Comment comment = commentService.createComment(dto);
+    return Response.ok(comment.getId());
+  }
+
+  @PatchMapping("/{commentId}")
+  public Response<Long> updateComment(@PathVariable Long commentId, @RequestBody UpdateCommentRequestDto dto) {
+    Comment comment = commentService.updateComment(commentId, dto);
+    return Response.ok(comment.getId());
+  }
+}
