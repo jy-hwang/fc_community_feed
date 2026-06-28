07c4f13147e4cb5441ad32184a5423e83529b617
jy-hwang <stratojy@gmail.com>
2026-06-28 21:17:28 +0900

    feat. UserPostQueue - publishPost 구현
    1. 내가 글을 작성하면
    2. 나를 팔로우 하고 있는 팔로워 목록을 가져오고
    3. List 로 반환하여 저장

--- src/main/java/org/fastcampus/post/repository/post_queue/UserPostQueueCommandRepositoryImpl.java
+++ src/main/java/org/fastcampus/post/repository/post_queue/UserPostQueueCommandRepositoryImpl.java
@@ -2,10 +2,14 @@ package org.fastcampus.post.repository.post_queue;
 
 import lombok.RequiredArgsConstructor;
 import org.fastcampus.post.repository.entity.post.PostEntity;
+import org.fastcampus.post.repository.entity.post.UserPostQueueEntity;
 import org.fastcampus.post.repository.jpa.JpaPostRepository;
 import org.fastcampus.post.repository.jpa.JpaUserPostQueueRepository;
+import org.fastcampus.user.repository.entity.UserEntity;
 import org.fastcampus.user.repository.jpa.JpaUserRelationRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,7 +20,15 @@ public class UserPostQueueCommandRepositoryImpl implements UserPostQueueCommandR
 
   @Override
   public void publishPost(PostEntity postEntity) {
+    UserEntity userEntity = postEntity.getAuthor();
+    List<Long> followerIds = jpaUserRelationRepository.findFollowers(userEntity.getId());
 
+    List<UserPostQueueEntity> userPostQueueEntityList
+        = followerIds.stream()
+        .map(userId -> new UserPostQueueEntity(userId, postEntity.getId(), userEntity.getId()))
+        .toList();
+
+    jpaUserPostQueueRepository.saveAll(userPostQueueEntityList);
   }
 
   @Override
--- src/main/java/org/fastcampus/user/repository/jpa/JpaUserRelationRepository.java
+++ src/main/java/org/fastcampus/user/repository/jpa/JpaUserRelationRepository.java
@@ -1,8 +1,15 @@
 package org.fastcampus.user.repository.jpa;
 
 import org.fastcampus.user.repository.entity.UserRelationEntity;
-import org.fastcampus.user.repository.entity.UserRelationIdEntity;
+import org.fastcampus.user.repository.entity.UserRelationId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface JpaUserRelationRepository extends JpaRepository<UserRelationEntity, UserRelationIdEntity> {
+import java.util.List;
+
+public interface JpaUserRelationRepository extends JpaRepository<UserRelationEntity, UserRelationId> {
+@Query(" SELECT ure.followingUserId" +
+    " FROM UserRelationEntity ure" +
+    " WHERE ure.followerUserId = :userId")
+  List<Long> findFollowers(Long userId);
 }
