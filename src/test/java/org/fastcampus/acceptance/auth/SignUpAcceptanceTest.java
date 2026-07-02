c7fc4ac5910a3b71ca277895569d83b4d2db48d5
jy-hwang <stratojy@gmail.com>
2026-07-02 21:30:37 +0900

    feat. 회원가입 이메일 인증 테스트 3
    1. test 작성 : 정상 이메일 주소 - 이메일 보내기 - 발급된 토큰 저장 확인

--- src/test/java/org/fastcampus/acceptance/auth/SignUpAcceptanceTest.java
+++ src/test/java/org/fastcampus/acceptance/auth/SignUpAcceptanceTest.java
@@ -1,11 +1,33 @@
 package org.fastcampus.acceptance.auth;
 
 import org.fastcampus.acceptance.utils.AcceptanceTestTemplate;
+import org.fastcampus.auth.application.dto.SendEmailRequestDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.fastcampus.acceptance.steps.SignUpAcceptanceSteps.requestSendEmail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class SignUpAcceptanceTest extends AcceptanceTestTemplate {
+  private final String email ="email-user@example.com";
+
   @BeforeEach
   void setup() {
     this.cleanUp();
+  }
+
+  @Test
+  void givenEmail_whenSendEmail_thenVerificationTokenSaved(){
+    // given
+    SendEmailRequestDto dto = new SendEmailRequestDto(email);
+
+    // when
+    Integer code = requestSendEmail(dto);
+
+    // then
+    String token = this.getEmailToken(email);
+    assertNotNull(token);
+    assertEquals(0, code);
   }
 }
